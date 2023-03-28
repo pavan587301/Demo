0b4d9e5180911fb3907674d055eda8752b3f75c4
pavan587301 <109473066+pavan587301@users.noreply.github.com>
2023-03-28 13:07:17 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,43 @@
+Create an ArrayList of integers and add some random values to it. Write a program to find the sum of all the elements in the ArrayList.
+
+Create an ArrayList of strings and add some names to it. Write a program to search for a specific name in the ArrayList.
+
+Create an ArrayList of doubles and add some random values to it. Write a program to find the average of all the elements in the ArrayList.
+
+Create an ArrayList of integers and add some random values to it. Write a program to remove all the even numbers from the ArrayList.
+
+Create an ArrayList of characters and add some random values to it. Write a program to count the number of vowels in the ArrayList.
+
+Create an ArrayList of strings and add some 
+sentences to it. Write a program to find the sentence with the maximum number of words in the ArrayList.
+
+Create an ArrayList of integers and add some random values to it. Write a program to sort the ArrayList in ascending order.
+
+Create an ArrayList of strings and add some names to it. Write a program to remove all the names that contain the letter 'a'.
+
+Create an ArrayList of integers and add some random values to it. Write a program to find the second largest element in the ArrayList.
+
+Create an ArrayList of strings and add some words to it. Write a program to find the longest word in the ArrayList.
+
+Finding the factorial of a number.
+Checking if a number is a prime number or not.
+Checking if a given string is a palindrome or not.
+Finding the GCD (Greatest Common Divisor) of two numbers.
+Finding the LCM (Least Common Multiple) of two numbers.
+Reversing an array.
+Finding the sum of the digits of a number.
+Checking if a given number is an Armstrong number or not.
+Finding the nth Fibonacci number.
+Sorting an array using bubble sort, selection sort, or insertion sort algorithms.
+Checking if a given year is a leap year or not.
+Checking if two strings are anagrams of each other.
+Finding the largest and smallest number in an array.
+Checking if a given number is a perfect square or not.
+Implementing binary search algorithm.
+Checking if a given number is a palindrome or not.
+Checking if a given number is a power of two or not.
+Reversing a string without using the reverse() method.
+Implementing stack and queue data structures.
+Finding the second largest element in an array.
+
+
